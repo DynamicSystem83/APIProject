7b2c552e97da37d4bbae6f9abd09dfb03d076cd7
Andy Yin <yindynasty@gmail.com>
2023-12-12 21:16:48 -0600

    adding links

--- src/main/java/edu/jhu/project/models/Theater.java
+++ src/main/java/edu/jhu/project/models/Theater.java
@@ -11,14 +11,20 @@ public class Theater
     private String name;
 	private Address addr;
 	private List<BusinessDay> businessHours;
+	private String selfRef;
+	private String moviesRef;
+	private String showingsRef;
 
-    public Theater(int theaterId, String name, Address addr, List<BusinessDay> businessHours)
+    public Theater(int theaterId, String name, Address addr, List<BusinessDay> businessHours, String baseURL)
     {
         super();
         this.theaterId = theaterId;
         this.name = name;
 		this.addr = addr;
 		this.businessHours = businessHours;
+		this.selfRef = baseURL + "/theaters/" + theaterId;
+		this.moviesRef = baseURL + "/theaters/" + theaterId + "/movies";
+		this.showingsRef = baseURL + "/theaters/" + theaterId + "/showings";
     }
 
     public int getTheaterId()
@@ -59,5 +65,35 @@ public class Theater
     public void setBusinessHours(List<BusinessDay> businessHours)
     {
         this.businessHours = businessHours;
+    }
+
+    public String getSelfRef()
+    {
+        return selfRef;
+    }
+
+    public void setSelfRef(String selfRef)
+    {
+        this.selfRef = selfRef;
+    }
+
+    public String getMoviesRef()
+    {
+        return moviesRef;
+    }
+
+    public void setMoviesRef(String moviesRef)
+    {
+        this.moviesRef = moviesRef;
+    }
+
+    public String getShowingsRef()
+    {
+        return showingsRef;
+    }
+
+    public void setShowingsRef(String showingsRef)
+    {
+        this.showingsRef = showingsRef;
     }
 }
